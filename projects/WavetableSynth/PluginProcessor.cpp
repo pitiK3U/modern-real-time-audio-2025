1a5af451429b122daef28a7cc30965a829fd3884
Daniel Ridzoň <daniel.ridzon@ackee.cz>
2025-06-05 21:57:15 +0300

    Fix back overflowing

--- projects/WavetableSynth/PluginProcessor.cpp
+++ projects/WavetableSynth/PluginProcessor.cpp
@@ -114,7 +114,8 @@ typename DSP::Parameter<FloatType>::EffectEvaluator getParameterEffect(Wavetable
     if (Param::ID::WavetablePosition.compare(settingParameter) == 0) {
         return [](float previousValue, float originalValue, float valueMultiplier, DSP::DSP<float>& dsp) {
                     auto value = previousValue + valueMultiplier * dsp.getCurrentValue();
-                    return std::fmod(value, Param::Ranges::WavetablePositionMax);
+                    auto max = Param::Ranges::WavetablePositionMax;
+                    return std::fmod(std::fmod(value, max) + max, max); // Wraps value in both directions into the range [0 - n]
                 };
     } else if (Param::ID::VCF_Cutoff.compare(settingParameter) == 0) {
         // const auto freqMod { std::clamp(vcfEnv * vcfEnvAmout + vcfLFOAmount * lfo, -1.f, 1.f) };
